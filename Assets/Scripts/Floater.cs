440b2c934d06d71c1eb6bc634687f8128835412f
Matt Lewis <matt@m-lewis.co.uk>
2020-10-04 22:31:30 +0100

    Add water and new player controls to main scene

--- Assets/Scripts/Floater.cs
+++ Assets/Scripts/Floater.cs
@@ -19,12 +19,15 @@ public class Floater : MonoBehaviour
     {
         WaveManager waveManager = surface.GetComponentInParent<WaveManager>();
 
+        // Add Gravity at floater position
         rigidBody.AddForceAtPosition(Physics.gravity / floaterCount, transform.position, ForceMode.Acceleration);
 
         float waveHeight = waveManager.GetWaveHeight(transform.position.x) + surface.position.y;
         if (transform.position.y < waveHeight)
         {
-            float displacementMultiplier = Mathf.Clamp01((waveHeight - transform.position.y) / depthBeforeSubmerged) * displacementAmount;
+            // Clamp Between 1 and 100 so it doesn't displace too much
+            float displacementMultiplier = Mathf.Clamp((waveHeight - transform.position.y) / depthBeforeSubmerged, 0, 100) * displacementAmount;
+
             rigidBody.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), transform.position, ForceMode.Acceleration);
             rigidBody.AddForce(displacementMultiplier * -rigidBody.velocity * waterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
             rigidBody.AddTorque(displacementMultiplier * -rigidBody.angularVelocity * waterAngularDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
